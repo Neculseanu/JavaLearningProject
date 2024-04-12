42c56836cf7ae21332b6891caca4b8ef9a8cc0ec
user <neculseanuiulian@gmail.com>
2024-04-12 20:09:19 +0300

    x2

--- src/main/java/variablesandcomments/CommentsAndVariables.java
+++ src/main/java/variablesandcomments/CommentsAndVariables.java
@@ -7,5 +7,8 @@ public class CommentsAndVariables
         boolean isHotOutside;
         isHotOutside = true;
         System.out.println(isHotOutside);
+        boolean isColdOutside;
+        isColdOutside = false;
+        System.out.println(isColdOutside);
     }
 }
